de13b6bb04faf8a7ba128d5825b368d7a527d9b6
Witty-Wizard <shashankmarch27@gmail.com>
2024-07-18 23:56:03 +0530

    [Added]: Images for dinosaur

--- src/Dino Game.lua
+++ src/Dino Game.lua
@@ -1,5 +1,42 @@
 Jump = false
-Duck= true
+Duck = true
+local pos = 0;
+local lastTriggerTime = 0 -- Variable to store the last trigger time
+local delay = 20          -- Delay in milliseconds (2 seconds)
+
+local function displayCactus1(xCord)
+    lcd.drawPixmap(xCord + 0, LCD_H - 38, "gfx/cactus_1_1.bmp")
+    lcd.drawPixmap(xCord + 45, LCD_H - 38, "gfx/cactus_1_2.bmp")
+end
+
+local function displayCactus2(xCord)
+    lcd.drawPixmap(xCord + 0, LCD_H - 38, "gfx/cactus_2_1.bmp")
+    lcd.drawPixmap(xCord + 45, LCD_H - 38, "gfx/cactus_2_2.bmp")
+end
+
+local function displayCactus3(xCord)
+    lcd.drawPixmap(xCord + 0, LCD_H - 38, "gfx/cactus_3_1.bmp")
+    lcd.drawPixmap(xCord + 45, LCD_H - 38, "gfx/cactus_3_2.bmp")
+end
+
+local function displayGround0(xCord)
+    lcd.drawPixmap(xCord + 0, LCD_H - 38, "gfx/ground_0_1.bmp")
+    lcd.drawPixmap(xCord + 45, LCD_H - 38, "gfx/ground_0_2.bmp")
+end
+
+local function displayGround1(xCord)
+    lcd.drawPixmap(xCord + 0, LCD_H - 38, "gfx/ground_1_1.bmp")
+    lcd.drawPixmap(xCord + 45, LCD_H - 38, "gfx/ground_1_2.bmp")
+end
+
+local function displayDino()
+    lcd.drawPixmap(5, LCD_H - 36, "gfx/dino_default.bmp")
+end
+
+local function init()
+    -- Initialization code if any
+    lastTriggerTime = getTime() -- Record the initial time
+end
 
 -- Main function
 local function run(event)
@@ -7,14 +44,21 @@ local function run(event)
         return 1
     end
 
+
+
     lcd.clear()
-    
+    local currentTime = getTime()
+    if (currentTime - lastTriggerTime) >= (delay / 10) then
+        pos = pos - 1
+        lastTriggerTime = currentTime
+    end
+
     local triggerValue = getSourceValue(1)
 
-    if triggerValue <=-100 then
+    if triggerValue <= -100 then
         Jump = false
         Duck = true
-    elseif triggerValue >=100 then
+    elseif triggerValue >= 100 then
         Jump = true
         Duck = false
     else
@@ -22,16 +66,20 @@ local function run(event)
         Duck = false
     end
 
-    if Jump then
-        lcd.drawPixmap(5,LCD_H-25, "gfx/dino_default.bmp")
-    elseif Duck then
-        lcd.drawPixmap(5,LCD_H-12, "gfx/dino_ducking_leftstep.bmp")
-    else
-        lcd.drawPixmap(5,LCD_H-18, "gfx/dino_default.bmp")
-    end
-    
+    -- if Jump then
+    --     lcd.drawPixmap(5,LCD_H-25, "gfx/dino_default.bmp")
+    -- elseif Duck then
+    --     lcd.drawPixmap(5,LCD_H-12, "gfx/dino_ducking_leftstep.bmp")
+    -- else
+    --     lcd.drawPixmap(5,LCD_H-18, "gfx/dino_default.bmp")
+    -- end
+    displayGround0(pos+90)
+    displayGround1(pos)
+    displayDino()
+
+
 
     return 0
 end
 
-return { run = run }
+return { init = init, run = run }
