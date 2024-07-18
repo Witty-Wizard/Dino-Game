9b3d91c357785faad87e71c2d134c89a8837a72d
Witty-Wizard <shashankmarch27@gmail.com>
2024-07-19 00:13:34 +0530

    [Added]: Jump Functionality

--- src/Dino Game.lua
+++ src/Dino Game.lua
@@ -1,36 +1,55 @@
-Jump = false
-Duck = true
-local pos = 0;
+local jump = false
+local duck = true
+local pos = 0
+local walk = true
 local lastTriggerTime = 0 -- Variable to store the last trigger time
 local delay = 20          -- Delay in milliseconds (2 seconds)
 
 local function displayCactus1(xCord)
-    lcd.drawPixmap(xCord + 0, LCD_H - 38, "gfx/cactus_1_1.bmp")
-    lcd.drawPixmap(xCord + 45, LCD_H - 38, "gfx/cactus_1_2.bmp")
+    lcd.drawPixmap(xCord + 0, LCD_H - 35, "gfx/cactus_1_1.bmp")
+    lcd.drawPixmap(xCord + 45, LCD_H - 35, "gfx/cactus_1_2.bmp")
 end
 
 local function displayCactus2(xCord)
-    lcd.drawPixmap(xCord + 0, LCD_H - 38, "gfx/cactus_2_1.bmp")
-    lcd.drawPixmap(xCord + 45, LCD_H - 38, "gfx/cactus_2_2.bmp")
+    lcd.drawPixmap(xCord + 0, LCD_H - 35, "gfx/cactus_2_1.bmp")
+    lcd.drawPixmap(xCord + 45, LCD_H - 35, "gfx/cactus_2_2.bmp")
 end
 
 local function displayCactus3(xCord)
-    lcd.drawPixmap(xCord + 0, LCD_H - 38, "gfx/cactus_3_1.bmp")
-    lcd.drawPixmap(xCord + 45, LCD_H - 38, "gfx/cactus_3_2.bmp")
+    lcd.drawPixmap(xCord + 0, LCD_H - 35, "gfx/cactus_3_1.bmp")
+    lcd.drawPixmap(xCord + 45, LCD_H - 35, "gfx/cactus_3_2.bmp")
 end
 
 local function displayGround0(xCord)
-    lcd.drawPixmap(xCord + 0, LCD_H - 38, "gfx/ground_0_1.bmp")
-    lcd.drawPixmap(xCord + 45, LCD_H - 38, "gfx/ground_0_2.bmp")
+    lcd.drawPixmap(xCord + 0, LCD_H - 35, "gfx/ground_0_1.bmp")
+    lcd.drawPixmap(xCord + 45, LCD_H - 35, "gfx/ground_0_2.bmp")
 end
 
 local function displayGround1(xCord)
-    lcd.drawPixmap(xCord + 0, LCD_H - 38, "gfx/ground_1_1.bmp")
-    lcd.drawPixmap(xCord + 45, LCD_H - 38, "gfx/ground_1_2.bmp")
+    lcd.drawPixmap(xCord + 0, LCD_H - 35, "gfx/ground_1_1.bmp")
+    lcd.drawPixmap(xCord + 45, LCD_H - 35, "gfx/ground_1_2.bmp")
 end
 
 local function displayDino()
-    lcd.drawPixmap(5, LCD_H - 36, "gfx/dino_default.bmp")
+    if duck then
+        if walk then
+            lcd.drawPixmap(5, LCD_H - 23, "gfx/dino_ducking_leftstep.bmp")
+        else
+            lcd.drawPixmap(5, LCD_H - 23, "gfx/dino_ducking_rightstep.bmp")
+        end
+    elseif jump then
+        if walk then
+            lcd.drawPixmap(5, LCD_H - 64, "gfx/dino_leftstep.bmp")
+        else
+            lcd.drawPixmap(5, LCD_H - 64, "gfx/dino_rightstep.bmp")
+        end
+    else
+        if walk then
+            lcd.drawPixmap(5, LCD_H - 36, "gfx/dino_leftstep.bmp")
+        else
+            lcd.drawPixmap(5, LCD_H - 36, "gfx/dino_rightstep.bmp")
+        end
+    end
 end
 
 local function init()
@@ -51,30 +70,29 @@ local function run(event)
     if (currentTime - lastTriggerTime) >= (delay / 10) then
         pos = pos - 1
         lastTriggerTime = currentTime
+        walk = not walk
     end
 
     local triggerValue = getSourceValue(1)
 
     if triggerValue <= -100 then
-        Jump = false
-        Duck = true
+        jump = false
+        duck = true
     elseif triggerValue >= 100 then
-        Jump = true
-        Duck = false
+        jump = true
+        duck = false
     else
-        Jump = false
-        Duck = false
+        jump = false
+        duck = false
     end
 
-    -- if Jump then
+    -- if jump then
     --     lcd.drawPixmap(5,LCD_H-25, "gfx/dino_default.bmp")
-    -- elseif Duck then
+    -- elseif duck then
     --     lcd.drawPixmap(5,LCD_H-12, "gfx/dino_ducking_leftstep.bmp")
     -- else
     --     lcd.drawPixmap(5,LCD_H-18, "gfx/dino_default.bmp")
     -- end
-    displayGround0(pos+90)
-    displayGround1(pos)
     displayDino()
 
 
